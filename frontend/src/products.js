f9fb0799a9ab84d0e32507e33fc334d21b3f873d
Serref <Reef163@gmail.com>
2023-07-18 13:20:18 -0400

    created home screen.

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "TRAVIS SCOTT X DUNK LOW PREMIUM QS SB 'CACTUS JACK'",
+    image: "/images/travis-scott-dunks.jpg",
+    description:
+      "Travis Scott teamed up with Nike to create the Travis Scott x SB Dunk Low PRM QS 'Cactus Jack.' Featuring a patchwork design, the shoe's upper emerges with a tan suede base accented by plaid on the quarter panel and paisley on the overlays. The overlays themselves are designed to wear away and reveal an elephant print, while the lateral side Swoosh sports a pink finish. Cactus Jack branding emerges on the tongue tag, while a rubber outsole is included for traction.",
+    brand: "Nike",
+    category: "Sneakers",
+    price: 1687.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "AIR YEEZY 2 SP 'RED OCTOBER'",
+    image: "/images/yeezy-red-octobers.jpg",
+    description:
+      "By the time the Air Yeezy 2 SP ‘Red October’ finally launched online with no advance notice in February 2014, Kanye West had already severed all ties with Nike. Though chaos surrounded the shoe’s much-delayed release, the Red Octobers stand out from its two companion colorways. There’s the monochromatic scarlet finish, contrasted only by metallic gold lace tips, and there’s the unique design, which sees the side panels ditch the standard anaconda texture in favor of rows upon rows of small triangular studs.",
+    brand: "Nike",
+    category: "Sneakers",
+    price: 11257.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "YEEZY 500 'BLUSH'",
+    image: "/images/yeezy-blush.jpg",
+    description:
+      "The Yeezy 500 first appeared as a pre-order in February 2018, before a wider release in April 2018. The 'Blush' colorway features a light taupe hue throughout the shoe, including the upper which is comprised of suede, mesh and leather. The sculpted midsole is lifted from the adidas KB8 III, which touted 'Feet You Wear' technology from the 1990s.",
+    brand: "Adidas",
+    category: "Sneakers",
+    price: 353.99,
+    countInStock: 5,
+    rating: 3.5,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "SACAI X LDWAFFLE 'VARSITY BLUE'",
+    image: "/images/nike-sacai.jpg",
+    description:
+      "Sacai and Nike teamed up to create the Sacai x LDWaffle 'Varsity Blue,' which debuted at Paris Fashion Week at the Sacai SP19 runway shoe. The hybrid look combines the Waffle Daybreak and LDV, resulting in two of each of the following: Swooshes, tongues, heel counters, eyestays and laces. The Varsity Red, Del Sol and Varsity Blue upper is built with a mix of suede, leather and mesh, while underfoot, the midsole includes an extension that goes past the heel. A waffle outsole provides traction, with Nike and Sacai branding on the heel and insole.",
+    brand: "Nike",
+    category: "Sneakers",
+    price: 514.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "AIR JORDAN 1 HIGH 'CHICAGO' 1985",
+    image: "/images/air-jordan-1-chicago.jpg",
+    description:
+      "When it first debuted in 1985, there weren’t grand expectations. Instead, there were critics loudly chirping about Nike’s decision to give Michael Jordan a large sneaker deal before he played a single game and the rookie’s choice to sign with a company that, up to that point, was best known for running shoes.",
+    brand: "Air Jordan",
+    category: "Sneakers",
+    price: 40000.0,
+    countInStock: 7,
+    rating: 5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "AIR JORDAN 7 RETRO 'OLYMPIC' 2012",
+    image: "/images/air-jordan-7-olympic.jpg",
+    description:
+      "The 2012 Air Jordan 7 'Olympic' is a retro release of Michael Jordan's shoe at the 1992 Olympic Games on the way to a gold medal with the Dream Team. The design features an upper white and metal silver with accents from Obsidian, metallic gold, and True Red. The sneaker released in July 2012 and also includes the Olympic Jersey number '9' on the heel. The 'Olympic' originally released in 2004 and also dropped in a 2016 'Tinker Alternate' colorway combining a white / navy upper with a sprinkled midsole.",
+    brand: "Air Jordan",
+    category: "Sneakers",
+    price: 5000.0,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
